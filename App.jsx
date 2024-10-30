22581e01cad97c6215c72f39149686351345f57c
devineeee <151719714+devineeee@users.noreply.github.com>
2024-10-30 10:37:48 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center bg-purple-700 min-h-screen">
+      <div>
+        <button
+          className="h-10 px-6 font-semibold rounded-md bg-white text-black"
+          type="submit"
+        >
+          Beli sekarang
+        </button>
+        <button
+          className="h-10 px-6 font-semibold rounded-md bg-yellow-600 text-black"
+          type="submit"
+        >
+          Beli sekarang
+        </button>
+        <button
+          className="h-10 px-6 font-semibold rounded-md bg-pink-600 text-black"
+          type="submit"
+        >
+          Beli sekarang
+        </button>
+      </div>
+    </div>
+  );
+}
